7419c442a487a8860ac19b11d6a7833b5ded5dc8
Meet Jain <meetj665@gmail.com>
2024-04-06 01:42:19 +0530

    week 4 3 . js

--- week4/week-4-3/week-4-3.js
+++ week4/week-4-3/week-4-3.js
@@ -20,4 +20,36 @@ const CourseSchema=new mongoose.Schema({
 const User = mongoose.model("User",UserSchema)
 const Course = mongoose.model("Course",CourseSchema)
 
+User.findById("1")
+User.findOne({username:"hkirat@gmail.com"})
+User.find({username:"hkirat@gmail.com"})
+User.updateOne(
+    {"id":"1"},
+    {$push:{purchasedCourses:"333"}}
+)
+User.updateOne(
+    {id:"1"},
+    {password:"newPassword"}
+)
+User.updateMany({},{
+    premium:true
+})
+User.deleteMany({})
+User.deleteOne({
+    username:"hkirat@gmail.com"
+})                           
+
+User.find
+User.findById
+User.findOne
+User.findByIdAndDelete
+User.findByIdAndUpdate
+User.findOneAndDelete
+User.findOneAndReplace
+User.findOneAndUpdate
+User.find
+User.updateMany
+User.updateOne
+
+
 app.listen (3000,()=>{console.log("listening on 3000")});
