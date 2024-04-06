11c8eb9c2492555eb14e8c01f81f7d2119bee494
Meet Jain <meetj665@gmail.com>
2024-04-06 17:24:17 +0530

    week 4-3/week3/mongoWithJwt

--- week4/week-4-3/week-3/04-mongo-with-jwt-auth/db/index.js
+++ week4/week-4-3/week-3/04-mongo-with-jwt-auth/db/index.js
@@ -1,19 +1,36 @@
 const mongoose = require('mongoose');
 
+const dotenv = require("dotenv")
+dotenv.config({path:"../.env"})
+
+mongoose.connect(process.env.MONGO_URL);
 // Connect to MongoDB
-mongoose.connect('your-mongodb-url');
+// mongoose.connect('your-mongodb-url');
 
 // Define schemas
 const AdminSchema = new mongoose.Schema({
     // Schema definition here
+    username:String,
+    password:String
 });
 
 const UserSchema = new mongoose.Schema({
     // Schema definition here
+    username:String,
+    password:String,
+    purchasedCourse:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:Course
+    }
 });
 
 const CourseSchema = new mongoose.Schema({
     // Schema definition here
+    title:String,
+    description:String,
+    price:Number,
+    imageLink:String,
+    published:Boolean
 });
 
 const Admin = mongoose.model('Admin', AdminSchema);
@@ -23,5 +40,6 @@ const Course = mongoose.model('Course', CourseSchema);
 module.exports = {
     Admin,
     User,
-    Course
+    Course,
+    jwtPassword:process.env.JWT_PASSWORD
 }
