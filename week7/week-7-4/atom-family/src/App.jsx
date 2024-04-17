834f9b7640e51681eeb3e88e38cfe943f6ba4c4d
Meet Jain <meetj665@gmail.com>
2024-04-17 13:37:02 +0530

    weej 7 3

--- week7/week-7-4/atom-family/src/App.jsx
+++ week7/week-7-4/atom-family/src/App.jsx
@@ -1,7 +1,115 @@
 
+// import './App.css'
+// import { RecoilRoot, useRecoilState, useRecoilValue , atom } from 'recoil';
+// import { todosAtomFamily } from './atoms';
+// const todoAtom = atom ({
+//   key:"todoAtom",
+//   default:{
+//     id: 1,
+//     title: "Go to Gym",
+//     description: "Hit the gym from 7-9"
+// }
+// })
+// function App() {
+//   return <RecoilRoot>
+//     <Todo id={1}/>
+//     <Todo id={2} />
+//   </RecoilRoot>
+// }
+
+// function Todo({id}) {
+//   const currentTodo = useRecoilValue(todoAtom)
+// return(
+//     <>
+//       {currentTodo.title}
+//       {currentTodo.description}
+//       <br />
+//     </>
+//   )
+// }
+// // function Todo({id}) {
+// //   const currentTodo = useRecoilValue(todoAtom)
+// //    const [todo, setTodo] = useRecoilState(todosAtomFamily(id));
+
+// //   return (
+// //     <>
+// //       {todo.title}
+// //       {todo.description}
+// //       <br />
+// //     </>
+// //   )
+// // }
+
+// export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import './App.css'
-import { RecoilRoot, useRecoilState } from 'recoil';
+import { RecoilRoot, useRecoilState, useSetRecoilState,useRecoilValue , atom } from 'recoil';
 import { todosAtomFamily } from './atoms';
+import { useEffect } from 'react';
 
 function App() {
   return <RecoilRoot>
@@ -11,11 +119,24 @@ function App() {
 }
 
 function Todo({id}) {
+
    const [todo, setTodo] = useRecoilState(todosAtomFamily(id));
+ const updateTodo = useSetRecoilState (todosAtomFamily(2))
+
+useEffect(()=>{
+  setTimeout (()=>{
+    updateTodo((todo)=>{
+      return{
+      id:todo.id+1,
+      title:"newTodo",
+      description:`newTodo ${parseInt(todo.id)  }`}
+    })
+  },2000)
+})
 
   return (
     <>
-      {todo.title}
+      {todo.title}<br / >
       {todo.description}
       <br />
     </>
