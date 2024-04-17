5765d286c5fedd331a9036d4150f013ae68baf0e
Meet Jain <meetj665@gmail.com>
2024-04-17 14:34:53 +0530

    week 7 3 done

--- week7/week-7-4/selector-family/src/App.jsx
+++ week7/week-7-4/selector-family/src/App.jsx
@@ -1,6 +1,45 @@
 
+// import './App.css'
+// import { RecoilRoot, useRecoilStateLoadable,useRecoilState } from 'recoil';
+// import { todosAtomFamily } from './atoms';
+
+// function App() {
+//   return <RecoilRoot>
+//     <Todo id={1}/>
+//     <Todo id={2} />
+//   </RecoilRoot>
+// }
+
+// function Todo({id}) {
+//   //  const [todo, setTodo] = useRecoilState(todosAtomFamily(id));
+//    const [todo, setTodo] = useRecoilState(todosAtomFamily(id));
+
+
+//   return (
+//     <>
+//       {todo.title}
+//       {todo.description}
+//       <br />
+//     </>
+//   )
+
+// }
+
+// export default App
+
+
+
+
+
+
+
+
+
+
+
+
 import './App.css'
-import { RecoilRoot, useRecoilState } from 'recoil';
+import { RecoilRoot, useRecoilStateLoadable,useRecoilState } from 'recoil';
 import { todosAtomFamily } from './atoms';
 
 function App() {
@@ -11,15 +50,26 @@ function App() {
 }
 
 function Todo({id}) {
-   const [todo, setTodo] = useRecoilState(todosAtomFamily(id));
+  //  const [todo, setTodo] = useRecoilState(todosAtomFamily(id));
+   const [todo, setTodo] = useRecoilStateLoadable(todosAtomFamily(id));
+console.log(todo.state)
+if(todo.state == "loading"){return (<div>Loading ...</div>)}else{
 
   return (
     <>
-      {todo.title}
-      {todo.description}
+      {todo.contents.title}
+      {todo.contents.description}
       <br />
     </>
   )
 }
+}
 
 export default App
+
+
+
+
+
+
+// learn by self suspence and error boundry
