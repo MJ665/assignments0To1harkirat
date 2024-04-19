38feeb4cfd75ce2961e0a2ab5c091348a7ae3861
Meet Jain <meetj665@gmail.com>
2024-04-20 01:14:13 +0530

    week 7 yt clone

---
+++ week8/week-8-1-youtube-clone/src/components/VideoCard.tsx
@@ -0,0 +1,21 @@
+export function VideoCard (){
+    return (
+        <div>
+
+            Hi from Video card html component
+            <img  src ="https://i.ytimg.com/vi/L8akCkfJj04/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAgsqJmg_Sw7hiGPHTN11AA6aFrOg"/>
+            <div className={"grid grid-cols-12 bg-red-500"}>
+                <div className={" col-span-2"}>
+            <img className={"rounded-full"} src ="https://i.ytimg.com/vi/L8akCkfJj04/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAgsqJmg_Sw7hiGPHTN11AA6aFrOg"/>
+            </div>
+            <div className={"col-span-10"}>
+                
+Jawan : chaleya (hindi)|
+Sharukh khan
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
