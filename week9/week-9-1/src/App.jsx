42c94b1723761af381b7aea1c70b7b26fc0e5894
Meet Jain <meetj665@gmail.com>
2024-05-02 22:01:19 +0530

    weeak a9

--- week9/week-9-1/src/App.jsx
+++ week9/week-9-1/src/App.jsx
@@ -63,6 +63,84 @@ import './App.css'
 
 
 
+// function App() {
+//   let [render ,  setRender] = useState(true)
+//   useEffect(()=>{
+//     setInterval(()=>{
+//       setRender(render = !render)
+//     },5000)
+//   })
+
+//   if(!render){
+//     return<div> this is second empty div</div>
+//   }else{return(<MyComponent></MyComponent>)}
+// }
+
+// function MyComponent (){
+// useEffect(()=>{console.log ("component mounted")//.  line a
+
+// return ()=>{  console.log("component unmounted")}// line B
+// },[])            
+// /*
+// the significance of the use Effect
+
+// if we have a use effect which have some dependencies 
+// then the first time dependency changes line a runns
+
+// the seconde time dependency changes first line b runs then line a runnns 
+// line b is for cleaning up and then line a runns
+
+// the third time the dependency change first line b runs for cleanup and then line a runns
+
+
+
+// in case of empty array 
+// then the components mounts on the DOM 
+// line a runns
+// then when the code goes away from the DOM
+// for cleanup 
+// line b runns
+
+// line b runns whenever the dependencies change or the component unmounts
+
+// hence the useEffect has 2 parts
+
+// line a ran when component mounted this is called life cycle events
+// line b ran when component unmounted this is called life cycle events
+
+
+
+
+// */
+//             return (
+//           <div>
+//            from inside my component
+//           </div>
+//             )
+//           }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// IN CLAASS BASED COMPONENT MOUNT AND UNMOUNT
+
+
 function App() {
   let [render ,  setRender] = useState(true)
   useEffect(()=>{
@@ -75,47 +153,24 @@ function App() {
     return<div> this is second empty div</div>
   }else{return(<MyComponent></MyComponent>)}
 }
-
-function MyComponent (){
-useEffect(()=>{console.log ("component mounted")//.  line a
-
-return ()=>{  console.log("component unmounted")}// line B
-},[])            
-/*
-the significance of the use Effect
-
-if we have a use effect which have some dependencies 
-then the first time dependency changes line a runns
-
-the seconde time dependency changes first line b runs then line a runnns 
-line b is for cleaning up and then line a runns
-
-the third time the dependency change first line b runs for cleanup and then line a runns
-
-
-
-in case of empty array 
-then the components mounts on the DOM 
-line a runns
-then when the code goes away from the DOM
-for cleanup 
-line b runns
-
-line b runns whenever the dependencies change or the component unmounts
-
-hence the useEffect has 2 parts
-
-line a ran when component mounted this is called life cycle events
-line b ran when component unmounted this is called life cycle events
+import React from 'react'
+class MyComponent extends React.Component{
+componentDidMount (){
+  console.log("the component mounted")
+}
+componentWillUnmount (){
+  console.log("the component unmounted")
+}
+render(){
+  return(
+  <div>
+      from inside my component
+          </div>)
+}
+}
 
 
 
 
-*/
-            return (
-          <div>
-           from inside my component
-          </div>
-            )
-          }
+
           export default App
