0ec556b54c333d165fbfcb670cdd32fd1ab35f94
Meet Jain <meetj665@gmail.com>
2024-05-05 03:08:00 +0530

    week 9 2

--- week9/week-9-2/index.ts
+++ week9/week-9-2/index.ts
@@ -1,3 +1,13 @@
+// to initialize a new project we have to
+// npm install -g typescript
+// npm init -y
+// tsc --init 
+// or npx tsc --init
+
+// we can do nodemon index.ts to run this type script files
+// also to transpile type script into the js and then run the code we have to run the command 
+// tsc -b
+
 console.log("hello")
 
 
