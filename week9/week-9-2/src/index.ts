71bb88528046b8d10fc01834f3f0ace63f439ffb
Meet Jain <meetj665@gmail.com>
2024-05-05 17:27:20 +0530

    week 9 2

--- week9/week-9-2/src/index.ts
+++ week9/week-9-2/src/index.ts
@@ -197,3 +197,136 @@ function greet (user:User):void{
 
 greet({firstName:"harkirat",lastName:"singh",age:20})
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+interface Person {
+    name:string;
+        age:number;
+    greet(phrase:string):void // Line a
+}
+class Employee implements Person { // Line. b
+    name:string ;
+    age :number;
+    constructor(n:string, a:number){
+        this.name = n
+        this.age= a
+    }
+    greet(phrase: string): void {
+        console.log(`${phrase}.  ${this.name}`)
+    }
+}
+class Manager1 { // Line c
+    name:string ;
+    age :number;
+    constructor(n:string, a:number){
+        this.name = n
+        this.age= a
+    }
+    greet(phrase: string): void {
+        console.log(`${phrase}.  ${this.name}`)
+    }
+}
+class Manager2 implements Person { // Line d
+    name:string ;
+    age :number;
+    constructor(n:string, a:number){
+        this.name = n
+        this.age= a
+    }
+    greet(phrase: string): void {
+        console.log(`${phrase}.  ${this.name}`)
+    }
+}
+class Manager3 implements Person { // Line e
+    name:string ;
+    age :number;
+    constructor(n:string, a:number){
+        this.name = n
+        this.age= a
+    }
+
+}
+const e = new Employee ("harkirat",22)
+console.log(e.name)
+e.greet("hello......")
+
+// interview question
+// what is difference between interfaces class and types in type script
+
+// what is meaning of implements Person
+// try to give a detailied explanation with example
+
+
+
+/*
+here the Line a  in person
+we have told each persong has a riight to get greet 
+so each person should be have greet function
+and every class implementing the person should have greet function
+
+
+now in employee implements person in LIne b
+we have told that the employee class is applied over Persons interface
+
+in Line c we have not told manager 1 we have not told implements person
+so it is possible that the manager cannot be a person 
+
+in line d we have told that manager2 implements the person
+and a person should have 
+name age and greet 
+so it is compulsory that manager should have defined variables like name age and greet
+
+in line e we have told manager 3 implements the person but we have removed the code of greet
+hence it is giving us the error of 
+Class 'Manager3' incorrectly implements interface 'Person'.
+  Property 'greet' is missing in type 'Manager3' but required in type 'Person'.ts(2420)
+index.ts(227, 5): 'greet' is declared here.
+          ^
+TSError: ⨯ Unable to compile TypeScript:
+src/index.ts:262:7 - error TS2420: Class 'Manager3' incorrectly implements interface 'Person'.
+  Property 'greet' is missing in type 'Manager3' but required in type 'Person'.
+
+262 class Manager3 implements Person { // Line e
+          ~~~~~~~~
+
+  src/index.ts:227:5
+    227     greet(phrase:string):void // Line a
+            ~~~~~~~~~~~~~~~~~~~~~~~~~
+    'greet' is declared here.
+
+    at createTSError (/opt/homebrew/lib/node_modules/ts-node/src/index.ts:859:12)
+    at reportTSError (/opt/homebrew/lib/node_modules/ts-node/src/index.ts:863:19)
+    at getOutput (/opt/homebrew/lib/node_modules/ts-node/src/index.ts:1077:36)
+    at Object.compile (/opt/homebrew/lib/node_modules/ts-node/src/index.ts:1433:41)
+    at Module.m._compile (/opt/homebrew/lib/node_modules/ts-node/src/index.ts:1617:30)
+    at Module._extensions..js (node:internal/modules/cjs/loader:1437:10)
+    at Object.require.extensions.<computed> [as .ts] (/opt/homebrew/lib/node_modules/ts-node/src/index.ts:1621:12)
+    at Module.load (node:internal/modules/cjs/loader:1212:32)
+    at Function.Module._load (node:internal/modules/cjs/loader:1028:12)
+    at Function.executeUserEntryPoint [as runMain] (node:internal/modules/run_main:142:12) {
+  diagnosticCodes: [ 2420 ]
+}
+
+*/
