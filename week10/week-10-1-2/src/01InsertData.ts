e1f202b34ea90f6b0d28cbb02708673e3df33bd8
Meet Jain <meetj665@gmail.com>
2024-05-08 03:53:03 +0530

    week 10 1 2

--- week10/week-10-1-2/src/01InsertData.ts
+++ week10/week-10-1-2/src/01InsertData.ts
@@ -1,6 +1,39 @@
 import { PrismaClient } from "@prisma/client";
+import { toUSVString } from "util";
+const prisma = new PrismaClient ()
 async function main() {
-    const prisma = new PrismaClient ()
-    
+    const user =await prisma.user.create({
+        data :{
+            email:"harkirat@gmail.com",
+            firstName:"harkirat",
+            lastName:"harkiratPassword",
+            password:"harkiratsPassword"
+        }, select:{
+            id:true, 
+            email:true,
+            password :true
+        }
+    })
+    const todo = await prisma.todo.create({
+        data:{
+            title:"harkirats Todo",
+            description :"harkiratsTodoDes",
+            done :false,
+            userId :user.id
+        },
+        select:{
+            id:true,
+            title:true,
+            userId:true
+        }
+    })
+    console.log(user)
+    console.log(todo)
 }
-main()
+main().then(async ()=>{
+    await prisma.$disconnect()
+}).catch(async (e)=>{
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+})
