055f735b4a219e62bfe7bacb1dc7904f9427c7d8
Meet Jain <meetj665@gmail.com>
2024-05-07 22:25:11 +0530

    week 10 1 1

--- week10/week-10-1-1/dist/02InsertData.js
+++ week10/week-10-1-1/dist/02InsertData.js
@@ -15,10 +15,10 @@ function main() {
         const client = (0, utils_1.getClient)();
         const userQuery = `insert into users (username , email, password) values ($1, $2 , $3) returning id`;
         const userData = ["hariOm", "hariom@gmail.com", "hariom"];
-        const user = (yield client).query(userQuery, userData);
+        const user = yield (yield client).query(userQuery, userData);
         console.log(user);
         const todoQuery = `insert into todos (title , description,userId) values ($1,$2,$3) returning id;`;
-        const todoData = ["hariOm's title", "hariOm's descriptiion'", user];
+        const todoData = ["hariOm's title", "hariOm's descriptiion", user.rows[0].id];
         const todo = (yield client).query(todoQuery, todoData);
         console.log("the data insertion successful");
     });
--- week10/week-10-1-1/src/02InsertData.ts
+++ week10/week-10-1-1/src/02InsertData.ts
@@ -1,13 +1,13 @@
 import { getClient } from "./utils";
 async function main() {
-    const client = getClient()
+    try{const client = getClient()
     const userQuery = `insert into users (username , email, password) values ($1, $2 , $3) returning id`
     const userData = ["hariOm", "hariom@gmail.com","hariom"]
     const user =await( await client).query(userQuery, userData)
     console.log(user)
     const todoQuery =`insert into todos (title , description,userId) values ($1,$2,$3) returning id;`
     const todoData = ["hariOm's title", "hariOm's descriptiion", user.rows[0].id]
-    const todo = (await client).query(todoQuery,todoData)
-    console.log("the data insertion successful")
+    const todo =await (await client).query(todoQuery,todoData)
+    console.log("the data insertion successful")}catch(err){console.error(err)}
 }
 main ()
