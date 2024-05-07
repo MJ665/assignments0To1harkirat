c9ce3219e1948a7cbc15faf1f58d9726a844c1d8
Meet Jain <meetj665@gmail.com>
2024-05-08 00:31:07 +0530

    week 10 1 1

---
+++ week10/week-10-1-1/src/03UpdateData.ts
@@ -0,0 +1,16 @@
+import { getClient } from "./utils";
+async function main() {
+    try{const client = getClient()
+    const userQuery = `update users set password $1 where email $2 `
+    const userData = ["hariOmNewPassword", "hariom@gmail.com",]
+    await( await client).query(userQuery, userData)
+    
+   
+    console.log("the data update successful")}catch(err){console.error(err)}
+}
+main ()
+
+
+
+
+
