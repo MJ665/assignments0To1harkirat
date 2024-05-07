ee54bed17214150a1f15a66c06304a30392d5c20
Meet Jain <meetj665@gmail.com>
2024-05-08 00:26:26 +0530

    week 10 1 1

---
+++ week10/week-10-1-1/src/04GetUser.ts
@@ -0,0 +1,19 @@
+import { getClient } from "./utils";
+async function main() {
+    try{const client = getClient()
+    const userQuery = `select * from users where email = $1`
+    const userData = ["hariom@gmail.com"]
+    const user =await( await client).query(userQuery, userData)
+    console.log(user)
+
+
+    console.log("the get data successful " + user.rows )}catch(err){console.error(err)}
+}
+main ()
+
+
+
+
+
+
+
