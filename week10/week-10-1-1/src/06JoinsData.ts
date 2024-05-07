f649746cd4e72134ad7314a09643555de401cca8
Meet Jain <meetj665@gmail.com>
2024-05-08 00:51:07 +0530

    week 10 1 1

--- week10/week-10-1-1/src/06JoinsData.ts
+++ week10/week-10-1-1/src/06JoinsData.ts
@@ -2,9 +2,9 @@ import { getClient } from "./utils";
 async function main() {
     try{const client = getClient()
     const joinQuery = `
-    select u.id u.username u.email a.title a.description a.id a.done 
+    select u.id, u.username, u.email, a.title, a.description, a.id, a.done 
     from users u join todos a on u.id = a.userId
-    where u.email = $1
+    where u.email = $1;
     `
     const joinData = ["hariom@gmail.com"]
     const user =await( await client).query(joinQuery,joinData)
