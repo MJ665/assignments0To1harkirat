cf14f5027e4f0a1e9927d53d6c54e0c306ec32af
Meet Jain <meetj665@gmail.com>
2024-05-09 04:40:32 +0530

    week 11 1 1

--- week11/week-11-1/my-app/src/index.ts
+++ week11/week-11-1/my-app/src/index.ts
@@ -13,6 +13,17 @@
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+		console.log(request.body);
+		console.log(request.headers);
+		
+		if (request.method === "GET") {
+			return Response.json({
+				message: "you sent a get request"
+			});
+		} else {
+			return Response.json({
+				message: "you did not send a get request"
+			});
+		}
 	},
 };
