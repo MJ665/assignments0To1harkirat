c8d0a1052a155593851c64be084732aba47e3207
Meet Jain <meetj665@gmail.com>
2024-05-23 22:55:28 +0530

    week 14

--- week14/week-14-2/src/app/page.tsx
+++ week14/week-14-2/src/app/page.tsx
@@ -39,6 +39,13 @@
 
 // but also ifii you have written use client on the top of the page then you cannot do this
 
+// next js lets you write bvackend routes just like express does
+
+// this s why next is considered to be a full stack frame workerData
+// the benefiats of using next js for backend routes in clueds 
+
+// code in single reportErrorall standart thing you get in a backend framewoork like isExpression
+// server components can directly talk to the backend
 
 
 
@@ -61,6 +68,8 @@
 import Image from "next/image";
 import axios from "axios";
 import { Component } from "react";
+import { workerData } from "worker_threads"
+import { isExpression } from "typescript"
 
 
 async function getUserDetails() {
@@ -68,7 +77,20 @@ async function getUserDetails() {
 
     setTimeout((r),5000)
   }) 
-  const response = await axios.get("https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details")
+  // const response = await axios.get("https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details")
+  
+  
+  
+   const response = await axios.get("http://localhost:3000/api/user")
+
+/* to make it to call our own backend api route folder 
+
+we have to just replace the url with our local host url 
+this is wrong for a few reason we can do the same thing in a better way in the future but for now we can do it in thisi way
+
+*/
+
+
   return response.data;
 }
 
