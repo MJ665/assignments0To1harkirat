c8296e5786c98aa606ce67364669869e006976c3
Meet Jain <meetj665@gmail.com>
2024-05-23 22:36:40 +0530

    week 14

---
+++ week14/week-14-2/src/app/loading.tsx
@@ -0,0 +1,9 @@
+
+export default function Loading (){
+    return (
+        <>
+        <div>
+            loading ...
+            </div></>
+    )
+}
--- week14/week-14-2/src/app/page.tsx
+++ week14/week-14-2/src/app/page.tsx
@@ -1,24 +1,28 @@
-import Image from "next/image";
-import axios from "axios";
-import { Component } from "react";
+// import Image from "next/image";
+// import axios from "axios";
+// import { Component } from "react";
 
 
-async function getUserDetails() {
-  const response = await axios.get("https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details")
-  return response.data;
-}
+// async function getUserDetails() {
+//   const response = await axios.get("https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details")
+//   return response.data;
+// }
 
-export default async function Home() {
-  const userDetail = await getUserDetails()
+// export default async function Home() {
+//   const userDetail = await getUserDetails()
   
-  return (
-    <> hi there
-    {userDetail.email}
-    {userDetail.name}
+//   return (
+//     <>   <div className="flex flex-col justify-center h-screen">
+//     <div className="flex justify-center"><div className="border p-8 rounded"><div>
+//       Name:{userDetail?.name}
+//       </div>
+//       {userDetail?.email}
+//       </div></div>
+//   </div>
     
-    </>
-  )
-}
+//     </>
+//   )
+// }
 
 
 
@@ -34,3 +38,58 @@ export default async function Home() {
 // next has recentely introduce that you can write async in the components 
 
 // but also ifii you have written use client on the top of the page then you cannot do this
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// now we are adding 5 second artificial delay to ocheck how to add loading on our site
+
+
+import Image from "next/image";
+import axios from "axios";
+import { Component } from "react";
+
+
+async function getUserDetails() {
+  await new Promise ((r)=>{
+
+    setTimeout((r),5000)
+  }) 
+  const response = await axios.get("https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details")
+  return response.data;
+}
+
+export default async function Home() {
+  const userDetail = await getUserDetails()
+  
+  return (
+    <>   <div className="flex flex-col justify-center h-screen">
+    <div className="flex justify-center"><div className="border p-8 rounded"><div>
+      Name:{userDetail?.name}
+      </div>
+      {userDetail?.email}
+      </div></div>
+  </div>
+    
+    </>
+  )
+}
+
+
+
+
+
+
