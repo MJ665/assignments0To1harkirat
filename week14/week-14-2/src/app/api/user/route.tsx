64206d04586b9e0451e8ff17d5336b932977613a
Meet Jain <meetj665@gmail.com>
2024-05-24 18:17:47 +0530

    week 14

--- week14/week-14-2/src/app/api/user/route.tsx
+++ week14/week-14-2/src/app/api/user/route.tsx
@@ -5,3 +5,5 @@ export function GET (){
     })
 }
 
+// we can make any file as api it is not necessary that it should have contained in the APi folder
+// we can make apis in any folder but they shoud be in the route tsx file name
---
+++ week14/week-14-2/src/app/signup/page.tsx
@@ -0,0 +1,7 @@
+import {Signup} from "@/components/Signup"
+export default function singup(){
+    return (
+        <>
+        <Signup></Signup></>
+    )
+}
