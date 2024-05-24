5c077685436bde87395cb46bd319f27e512006c7
Meet Jain <meetj665@gmail.com>
2024-05-24 18:58:55 +0530

    week 14

--- week14/week-14-2/src/app/api/user/route.tsx
+++ week14/week-14-2/src/app/api/user/route.tsx
@@ -21,10 +21,17 @@
 
 
 import {NextRequest} from "next/server"
+import { PrismaClient} from "@prisma/client"
+const client = new PrismaClient()
 export async function  POST(req:NextRequest) {
     // extract the body
     const body = await req.json()
     // store the body in the Database
+    client.user.create({
+        data :{
+        username :body.username,
+        password :body.password}
+    })
     console.log(body)
     return Response.json({
         msg:"you are logged in!"
