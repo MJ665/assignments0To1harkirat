f8fefce93f32d891791d77f5375976044e25b0b5
Meet Jain <meetj665@gmail.com>
2024-05-25 02:59:17 +0530

    week 14 3

--- week14/week-14-3/src/app/api/user/route.tsx
+++ week14/week-14-3/src/app/api/user/route.tsx
@@ -5,12 +5,19 @@ export  function GET ()
     name :"harkirat"
 }))}
 
-
+import { PrismaClient} from "@prisma/client"
+ const client = new PrismaClient()
 export async function POST (req:NextRequest)
 
 {
     
     const body = await req.json()
+    client.user.create({
+        data :{
+            email:body.username,
+            name:body.password
+        }
+    })
     // console.log(req.headers.get("authorization"))
     // console.log(req.nextUrl.searchParams.get("name"))
     return (NextResponse.json ({
