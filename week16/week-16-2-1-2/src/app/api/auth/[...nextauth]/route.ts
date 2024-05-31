8d145f589c5741ef6ef467c3f14e2076cf72db05
Meet Jain <meetj665@gmail.com>
2024-06-01 02:20:19 +0530

    week 16

--- week16/week-16-2-1-2/src/app/api/auth/[...nextauth]/route.ts
+++ week16/week-16-2-1-2/src/app/api/auth/[...nextauth]/route.ts
@@ -53,28 +53,10 @@
 
 
 
-
-
-
-import NextAuth from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
+import NextAuth from "next-auth"
 
 const handler = NextAuth({
-    providers: [
-        CredentialsProvider({
-            name: "Email",
-            credentials: {
-                username: { label: 'email', type: "text", placeholder: 'Email' },
-                password: { label: 'password', type: "password", placeholder: 'Password' },
-            },
-            async authorize(credentials) {
-                return {
-                    id: "user1"
-                };
-            }
-        })
-    ]
-});
 
-// Export only the POST handler
-export const POST = handler;
+})
+
+export { handler as GET, handler as POST }
