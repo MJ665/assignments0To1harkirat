ef5b6852a16bb8216c7cb02dd18411e47e02a836
Meet Jain <meetj665@gmail.com>
2024-06-01 01:50:00 +0530

    week 16 1  2  2 1 1

--- week16/week-16-2-1-2/src/app/api/auth/[...authRoutes]/route.ts
+++ week16/week-16-2-1-2/src/app/api/auth/[...authRoutes]/route.ts
@@ -1,29 +1,45 @@
-import { NextRequest, NextResponse } from "next/server";
+// import { NextRequest, NextResponse } from "next/server";
 
-export function GET (){
-    return NextResponse.json({
-        message:"asd"
-    })
-}
-export function POST (req:NextRequest){
-    return NextResponse.json({
-        message:"asd"
-    })
-}
+// // export function GET (req:NextRequest , args:any){
+// //     console.log (args.params.authRoutes)
+// //     return NextResponse.json({
+// //         message:"asd"
+// //     })
+// // }
+// export function GET(
+//   req: NextRequest,
+//   { params: { authRoutes } }: { params: { authRoutes: string[] } }
+// ) {
+//   console.log(authRoutes);
+//   return NextResponse.json({
+//     message: "asd",
+//   });
+// }
+// export function POST(req: NextRequest) {
+//   return NextResponse.json({
+//     message: "asd",
+//   });
+// }
+
+// // if i have tot make it some thing like
+// // get or post request in
+// // http://localhost:3000/api/auth
+
+// // then i will put my route . ts file in
+// // src/api/auth/route.ts
+
+// // but here i want it in a way that
+// // http://localhost:3000/api/auth/anything
+
+// // thats why my folder structure is
+// // src/api/auth/[...authRoutes]/route.ts
 
 
-if i have tot make it some thing like 
-get or post request in 
-http://localhost:3000/api/auth
-
-then i will put my route . ts file in 
-src/api/auth/route.ts
 
 
 
 
-but here i want it in a way that 
-http://localhost:3000/api/auth/anything
 
-thats why my folder structure is 
-src/api/auth/[...authRoutes]/route.ts
+
+
+
