9cc3e60f7453c1eadeaf8396d682a3e791095f2d
Meet Jain <meetj665@gmail.com>
2024-06-01 02:26:25 +0530

    week 16

--- week16/week-16-2-1-2/src/app/api/auth/[...nextauth]/route.ts
+++ week16/week-16-2-1-2/src/app/api/auth/[...nextauth]/route.ts
@@ -52,11 +52,27 @@
 
 
 
-
 import NextAuth from "next-auth"
+import CredentialsProvider from 'next-auth/providers/credentials';
 
 const handler = NextAuth({
-
+  providers: [
+    CredentialsProvider({
+        name: 'Credentials',
+        credentials: {
+          username: { label: 'email', type: 'text', placeholder: '' },
+          password: { label: 'password', type: 'password', placeholder: '' },
+        //   otp: { label: 'otp', type: 'otp', placeholder: '' },
+        },
+        async authorize(credentials: any) {
+            
+            return {
+                id: "user1"
+            };
+        },
+      })
+  ],
+  secret: process.env.NEXTAUTH_SECRET
 })
 
 export { handler as GET, handler as POST }
---
+++ week16/week-16-2-1-2/src/app/signup/page.tsx
@@ -0,0 +1,12 @@
+export default function signup (){
+    return (
+        <>
+        <div>
+            hello signup page
+            
+            <input type=" text" placeholder=" email" />
+            <input type=" text" placeholder=" password " />
+            <button> submit </button>
+            </div></>
+    )
+}
