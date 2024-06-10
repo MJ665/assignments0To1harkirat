6ff146923bc936db36a0e720d69c0c899e0b91d2
Meet Jain <meetj665@gmail.com>
2024-06-10 23:14:25 +0530

    week 12 6

--- week12/week-12-6/src/index.ts
+++ week12/week-12-6/src/index.ts
@@ -1,9 +1,30 @@
-import { Hono } from 'hono'
+import { Hono, Next } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import { env } from 'hono/adapter'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.post('/', async (c) => {
+  // Todo: add Zod validation here
+  const body = await c.req.json()
+  const { DATABASE_URL } = env<{ DATABASE_URL: string }>(c)
+
+  const prisma = new PrismaClient({
+      datasourceUrl: DATABASE_URL,
+  }).$extends(withAccelerate())
+
+  console.log(body)
+
+  await prisma.user.create({
+    data: {
+      name: body.name,
+      email: body.email,
+      password: body.password
+    }
+  })
+
+  return c.json({msg: "User created"})
 })
 
 export default app
